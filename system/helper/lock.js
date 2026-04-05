ea3ec69580c9a7b63c339a95c87cfc33cd8b1e9c
FebryWeskerv2 <vandebry10@gmail.com>
2026-04-05 18:18:52 +0800

    Update lock.js

--- system/helper/lock.js
+++ system/helper/lock.js
@@ -1,19 +1,3 @@
-/* ════════════════════════════════════════════
- * Wesker-MD  ╌  febry wesker
- * ════════════════════════════════════════════
- * file    : system/helper/lock.js
- * desc    : system › helper › lock
- * author  : febry  ⪩  2026
- * ════════════════════════════════════════════
- * © 2026 febry wesker. all rights reserved.
- * do not resell, redistribute, or claim as
- * your own work without explicit permission.
- * ────────────────────────────────────────────
- * © 2026 febry wesker. semua hak dilindungi.
- * dilarang menjual, menyebarkan, atau mengaku
- * sebagai karya sendiri tanpa izin tertulis.
- * ════════════════════════════════════════════ */
-
 import path from 'node:path'
 import { fileURLToPath } from 'url'
 import { ConfigCache } from './config-cache.js'
